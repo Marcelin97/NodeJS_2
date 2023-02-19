93d05b5e63345f567467691822c4db9bc4f6de74
Marcelin97 <lois_m@outlook.com>
2023-02-19 18:29:41 +0100

    install nodemon and change command to start my application

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const port = 3000
 
 // premier point de terminaison
 // la méthode ici est "get" est qui prend en paramètre 2 éléments
-// 1 élément : le chemin de la requête / le chemin de la route ici c'est la route par défault '/'
+// 1 élément : le chemin de la requête / le chemin de la route ici c'est la route par défaut '/'
 // 2 c'est une fonction qui à pour rôle de fournir une réponse au client lorsque notre point de terminaison est appelé. Cette fonction a elle même deux arguments (req et res)
 // L'argument req permet de récupérer un objet request correspondant à la requête reçu en entrée par notre point de terminaison
 // l'objet res correspond à la response, c'est l'object que l'on doit renvoyé à notre client
-app.get('/', (req, res) => res.send('Hello, Express ! '))
+app.get('/', (req, res) => res.send('Hello, Express !'))
 
 // je démarre l'api rest sur le port 3000 et j'affiche un message
 app.listen(port,()=> console.log(`Notre application Node est démarrée sur : http://localhost:${port}`) )
