379e99b76f5ec4d52e4a109f81cf8033a58d7949
Marcelin97 <lois_m@outlook.com>
2023-03-31 09:54:40 +0200

    :sparkles: start to add a pokemon with a new request | method post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
-const { success } = require("./helper.js"); // je récupère uniquement la méthode success
+const { success, getUniqueId } = require("./helper.js"); // je récupère uniquement la méthode success
 let pokemons = require("./mock.pokemon.js"); // j'importe ma liste de pokémons
 
 // on crée une instance d'une application express
@@ -29,6 +29,14 @@ app.get("/", (req, res) => res.send("Hello, Express !"));
 // définition d'un point de terminaison
 // Point de terminaison Express = app.METHODE(CHEMIN, GESTIONNAIRE(req, res))
 
+// * GET ALL POKEMONS
+app.get("/api/pokemons/", (req, res) => {
+    // res.send(`Il y a ${pokemons.length} pokémons dans le pokédex`)
+    const message = "Voici la liste de tous les pokémons";
+    res.json(success(message, pokemons));
+  });
+
+// * GET A POKEMON
 app.get("/api/pokemons/:id", (req, res) => {
   // j'utilise ma liste de pokemon que j'ai importé ligne 3
   const id = parseInt(req.params.id); // récupérer un paramètre dans une url grâce la propriété params de la requête
@@ -37,12 +45,17 @@ app.get("/api/pokemons/:id", (req, res) => {
   res.json(success(message, pokemon));
 });
 
-app.get("/api/pokemons/", (req, res) => {
-  // res.send(`Il y a ${pokemons.length} pokémons dans le pokédex`)
-  const message = "Voici la liste de tous les pokémons";
-  res.json(success(message, pokemons));
-});
 
+// * ADD A POKEMON
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons)
+
+    // Via Postman je récupère le body sous le format d'une string et non un JSON => l'object est de transformer nos donnée en string en JSON
+    const pokemonCreated = { ...req.body, ...{id: id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokémon ${pokemonCreated.name} a bien été crée.`
+    res.json(success(message, pokemonCreated))
+  })
 // je démarre l'api rest sur le port 3000 et j'affiche un message
 app.listen(port, () =>
   console.log(
